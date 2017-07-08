828129cbb03072b84dcf9a4255fa71c7581a152d
Jared Forsyth <jared@jaredforsyth.com>
2017-07-08 11:21:14 -0600

    initial

---
+++ src/html.js
@@ -0,0 +1,53 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+
+import { TypographyStyle } from "react-typography"
+import typography from "./utils/typography"
+
+const BUILD_TIME = new Date().getTime()
+
+export default class HTML extends React.Component {
+  static propTypes = {
+    body: PropTypes.string,
+  }
+
+  render() {
+    const head = Helmet.rewind()
+
+    let css
+    if (process.env.NODE_ENV === "production") {
+      css = (
+        <style
+          dangerouslySetInnerHTML={{
+            __html: require("!raw!../public/styles.css"),
+          }}
+        />
+      )
+    }
+
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="stylesheet" href="reset.css"/>
+          {this.props.headComponents}
+          <TypographyStyle typography={typography} />
+          {css}
+        </head>
+        <body>
+          <div
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+}
---
+++ src/pages/talk.md
@@ -0,0 +1,5 @@
+---
+path: /talk.html
+---
+# Hello folks
+The water's warm
