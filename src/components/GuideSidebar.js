3d148889224f84c46bf27754e6bccd96e0712a27
Jared Forsyth <jared@jaredforsyth.com>
2017-07-10 23:29:49 -0600

    mobile-friendly nav

--- src/components/GuideSidebar.js
+++ src/components/GuideSidebar.js
@@ -4,9 +4,23 @@ import Link from './Link'
 import {accent} from '../utils/colors'
 
 export default class GuideSidebar extends React.Component {
+  state = {closed: true}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.current !== this.props.current) {
+      this.setState({closed: true})
+    }
+  }
   render() {
     return <div css={styles.container}>
-      <Node current={this.props.current} item={this.props.root} root depth={0} />
+      <div
+        css={styles.navToggle}
+        onClick={() => this.setState({closed: !this.state.closed})}
+      >
+        Navigation
+      </div>
+      <div css={[styles.contents, this.state.closed && styles.hiddenContents]}>
+        <Node current={this.props.current} item={this.props.root} root depth={0} />
+      </div>
     </div>
   }
 }
@@ -28,6 +42,31 @@ const styles = {
     width: 200,
     fontSize: 14,
     lineHeight: '14px',
+    '@media(max-width: 800px)': {
+      width: 'auto',
+    },
+  },
+
+  contents: {
+    padding: '2em',
+  },
+
+  hiddenContents: {
+    '@media(max-width: 800px)': {
+      display: 'none',
+    }
+  },
+
+  navToggle: {
+    backgroundColor: '#444',
+    color: 'white',
+    padding: '1em 2em',
+    alignSelf: 'stretch',
+    display: 'none',
+    cursor: 'pointer',
+    '@media(max-width: 800px)': {
+      display: 'flex',
+    }
   },
 
   node: {
@@ -35,12 +74,12 @@ const styles = {
   },
 
   li: {
-    marginBottom: 10,
   },
 
   link: {
     textDecoration: 'none',
     color: 'currentColor',
+    paddingBottom: 10,
   },
 
   currentLink: {
@@ -52,7 +91,7 @@ const styles = {
   link1: {
     fontWeight: 'bold',
     fontSize: '1.2em',
-    marginBottom: 10,
+    paddingBottom: 10,
   },
 
   rootLink: {
