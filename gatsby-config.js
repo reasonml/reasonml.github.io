9b273157334232336617ec3f7a8feb535bf41691
Cheng Lou <chenglou92@gmail.com>
2017-07-18 15:00:44 -0700

    Add anchor links to sub-sections
    Fixes #2

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
               maxWidth: 690,
             },
           },
+          'gatsby-remark-autolink-headers',
           // {
           //   resolve: 'gatsby-remark-responsive-iframe',
           // },
