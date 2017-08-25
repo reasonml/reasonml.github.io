db3a96504afcf59a6152b5267e0033fedd1daf27
Cheng Lou <chenglou92@gmail.com>
2017-08-25 01:44:12 -0700

    minify stdlibBundle

--- regenerateStdlibForBrowser.js
+++ regenerateStdlibForBrowser.js
@@ -12,7 +12,7 @@ const browserify = require('browserify');
 
 const b = browserify();
 
-const stdlibDir = path.join(__dirname, 'static', 'stdlib');
+const stdlibDir = path.join(__dirname, 'static', 'js');
 
 let stdlibDirFiles;
 try {
@@ -21,7 +21,7 @@ try {
   console.log(`
 ** There's an error while reading ${stdlibDir}. Is this your first time setting things up?
 You need to copy the BuckleScript standard library files from here:
-https://github.com/BuckleScript/bucklescript/tree/d21cdfbbe61afb0ae6b79735765de949409947ea/lib/js
+https://github.com/BuckleScript/bucklescript/tree/47e8fea03391b26d27dd7fe00fd6cb5e88eafc58/lib/js
 into ${stdlibDir}.
 (To upgrade the stdlib, use master's files)
 This script will then call Browserify to bundle each file, while exposing them correctly in a global \`require\` you can use in the browser.`);
@@ -34,4 +34,7 @@ stdlibDirFiles.forEach(file => {
   b.require(path.join(stdlibDir, file), {expose: exposedRequireName});
 });
 
-b.bundle().pipe(fs.createWriteStream(path.join(__dirname, 'static', 'stdlibBundle.js')));
+b
+  .transform('uglifyify')
+  .bundle()
+  .pipe(fs.createWriteStream(path.join(__dirname, 'static', 'stdlibBundle.js')));
