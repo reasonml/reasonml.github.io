5283ea646243fccd69b0ab332eedb8dd84f29f67
Cheng Lou <chenglou92@gmail.com>
2017-09-07 01:03:52 -0700

    Updates

---
+++ evalWorker.js
@@ -0,0 +1,14 @@
+importScripts('stdlibBundle.js');
+
+const _console = console;
+
+console = {
+  log: (...items) => postMessage({ type: 'log', contents: items }),
+  error: (...items) => postMessage({ type: 'error', contents: items }),
+  warn: (...items) => postMessage({ type: 'warn', contents: items })
+};
+
+onmessage = ({data}) => {
+  eval(data.code);
+  postMessage({ type: 'end', contents: data.timerId });
+};
