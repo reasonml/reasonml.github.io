b7966483ac066b1bf307c267ae6df7b5921c9116
Cheng Lou <chenglou92@gmail.com>
2017-09-08 17:09:37 -0700

    Updates

---
+++ js/js_null_undefined.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+function bind(x, f) {
+  if (x == null) {
+    return x;
+  } else {
+    return f(x);
+  }
+}
+
+function iter(x, f) {
+  if (x == null) {
+    return /* () */0;
+  } else {
+    return f(x);
+  }
+}
+
+function from_opt(x) {
+  if (x) {
+    return x[0];
+  } else {
+    return undefined;
+  }
+}
+
+exports.bind     = bind;
+exports.iter     = iter;
+exports.from_opt = from_opt;
+/* No side effect */
---
+++ stdlib/js_null.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+function bind(x, f) {
+  if (x !== null) {
+    return f(x);
+  } else {
+    return null;
+  }
+}
+
+function iter(x, f) {
+  if (x !== null) {
+    return f(x);
+  } else {
+    return /* () */0;
+  }
+}
+
+function from_opt(x) {
+  if (x) {
+    return x[0];
+  } else {
+    return null;
+  }
+}
+
+exports.bind     = bind;
+exports.iter     = iter;
+exports.from_opt = from_opt;
+/* No side effect */
