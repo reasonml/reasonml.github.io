af450e034508145b2cf7e739fee8096fc4e4b816
Cheng Lou <chenglou92@gmail.com>
2017-09-13 23:17:27 -0700

    Updates

--- regenerateStdlibForBrowser.sh
+++
@@ -1,6 +0,0 @@
-const browserify = require('browserify');
-
-# assuming you're in this directory, and installed all the dependencies for this project in the parent directory through `yarn`
-../node_modules/.bin/browserify
-
--r
