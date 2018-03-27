05d66500f219559fb6f9b3506114a84fe121f2bf
Cheng Lou <chenglou92@gmail.com>
2018-03-26 22:09:33 -0700

    Temporarily remove cleanslate.css
    cleanslate also disabled styling for ReasonReact components since the
    former has a hard precendence for styling on everything. The solution
    for a proper reset is to put the content inside an iframe; though that
      gets troublesome too for other reasons.

--- website/pages/en/try.js
+++ website/pages/en/try.js
@@ -3,7 +3,6 @@ const React = require("react");
 class Index extends React.Component {
   render() {
     return <span>
-      <link rel="stylesheet" type="text/css" href="/css/cleanslate.css" />
       <div id="try-wrapper" />
       <script type="text/javascript" src="/js/bsReasonReact.js" />
       <script type="text/javascript" src="/js/refmt.js" />
