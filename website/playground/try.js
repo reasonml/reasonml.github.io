0bcf4f82381d5fb8e227efcb3bcfa344dd245281
Blaine Bublitz <blaine.bublitz@gmail.com>
2019-04-22 17:22:32 +0200

    add hooks example (not working yet because old react)

--- website/playground/try.js
+++ website/playground/try.js
@@ -219,6 +219,26 @@ module Greeting = {
 
 ReactDOMRe.renderToElementWithId(<Greeting />, "preview");`
 }, {
+  name: 'React Hooks',
+  code:
+`[@bs.config {jsx: 3}];
+
+module Counter = {
+  [@react.component]
+  let make = (~name) => {
+    let (count, setCount) = React.useState(() => 0);
+
+    <div>
+      <p> {React.string(name ++ " clicked " ++ string_of_int(count) ++ " times")} </p>
+      <button onClick={_ => setCount(_ => count + 1)}>
+        {React.string("Click me")}
+      </button>
+    </div>
+  };
+};
+
+ReactDOMRe.renderToElementWithId(<Counter name="Counter" />, "preview");`
+}, {
   name: '(Legacy Record API) ReasonReact Greetings',
   code:
 `module Greeting = {
