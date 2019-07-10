3f9d6e2d05720c9390975932d8823688efe5f2e4
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-07-09 20:48:04 -0500

    reason conf us 2019 advert

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -108,14 +108,11 @@ class HomeSplash extends React.Component {
             <div className="projectTitle">
               <img alt={siteConfig.title} src={`${siteConfig.baseUrl}img/reason.svg`} />
             </div>
-
             {/* Keep this section for future events! */}
-            {/*
             <div className="homeWrapperPromo">
-              ReasonConf 2019 is happening in Europe on April 11th - 13th.<br/>
-              <a href="https://www.reason-conf.com" target="_blank"> Get your tickets soon! </a>
+              ReasonConf US 2019 is happening in Chicago on October 8th!<br/>
+              <a href="https://www.reason-conf.us" target="_blank"> Get your tickets soon! </a>
             </div>
-            */}
             <div className="homeWrapperInner">
               <div className="homeCodeSnippet">
                 <MarkdownBlock>{codeExampleSmallScreen}</MarkdownBlock>
