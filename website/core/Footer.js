21ce6f467f953500a2191148f342446b7b5fee1b
Cheng Lou <chenglou92@gmail.com>
2020-03-02 20:55:08 -0800

    Temporarily remove pjax
    cc @rickyvetter
    
    This has been causing a bunch of problems because docusaurus assumes
    that some of its scripts, e.g. code tab click events, can just be set up
    once, after the window loads. Pjax violates those assumptions. Plus, the
    anchoring mechanism now errors in conjunction with docusaurus's scrollSpy.
    Go to `types` page, refresh (for a real full fetch), then go to `string &
    char` page then scroll, to see the errors in the console each scroll.
    
    I'm not sure what to do here. It's really unforunate that docusaurus'
    assumptions of page loading afresh each time collides with pjax's
    
    To remove the buggy behaviors, especially since they're preventing code
    tabs switching to work, I'm disabling pjax for now.

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -7,28 +7,6 @@ class Footer extends React.Component {
     return (
       <span>
         <script src={this.props.config.baseUrl + 'js/redirectBlog.js'}></script>
-        <script src={this.props.config.baseUrl + 'js/pjax-api.min.js'}></script>
-        <script dangerouslySetInnerHTML={{__html: `
-          var Pjax = require('pjax-api').Pjax;
-          new Pjax({
-            areas: [
-              // try to use the first query.
-              '.mainContainer, .docsNavContainer .toc .navWrapper, .onPageNav',
-              // fallback
-              'body'
-            ],
-            link: '.docsNavContainer:not(.docsSliderActive) a',
-            update: {
-              script: false,
-            }
-          });
-          var languagesMenuItemCopy = document.getElementById("languages-menu");
-          languagesMenuItemCopy.addEventListener("click", function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            e.stopImmediatePropagation();
-          });
-        `}}></script>
       </span>
     );
   }
