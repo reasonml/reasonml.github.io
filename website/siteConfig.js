c46fe8c18524477a0e6490cde5dd5597f93d0b2b
Cheng Lou <chenglou92@gmail.com>
2020-08-26 06:53:28 -0700

    Redirect JS docs when relevant

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -354,6 +354,7 @@ const siteConfig = {
   users,
   examples,
   onPageNav: 'separate',
+  scripts: ['/js/redirectToNewDoc.js'],
   /* path to images for header/footer */
   headerIcon: "img/dummy.svg",
   footerIcon: "img/icon_50.png",
